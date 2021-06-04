120f4468de12547bf72c552549eb6e398aaba729
stefanocoretta <stefano.coretta@gmail.com>
2021-06-04 16:27:01 +0200

    🔥 - clean up

--- README.md
+++ README.md
@@ -1,27 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# bayes.handson
+# learnB4SS
 
 <!-- badges: start -->
 <!-- badges: end -->
-
-The goal of bayes.handson is to …
-
-## Installation
-
-You can install the released version of bayes.handson from
-[CRAN](https://CRAN.R-project.org) with:
-
-``` r
-install.packages("bayes.handson")
-```
-
-## Example
-
-This is a basic example which shows you how to solve a common problem:
-
-``` r
-library(bayes.handson)
-## basic example code
-```
--- data-raw/prep-data.R
+++ data-raw/prep-data.R
@@ -4,18 +4,6 @@ library(tidyverse)
 
 dataset <- read_csv(here::here("data-raw/dataset.csv"))
 
-# To be filled later, don't remove.
-# attr(songs, "metadata") <- list(
-#   url = "https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks",
-#   license = list(
-#     name = "Community Data License Agreement - Sharing - Version 1.0",
-#     url = "https://cdla.io/sharing-1-0/"
-#   ),
-#   sources = "Spotify Web API",
-#   dataset_owner = "Yamaç Eren Ay",
-#   version = "9"
-# )
-
 usethis::use_data(dataset, overwrite = TRUE)
 
 
