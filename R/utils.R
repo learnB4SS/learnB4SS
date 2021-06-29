88f300b5628ecaa5589f62e1c2e0524ed7574bfa
stefanocoretta <stefano.coretta@gmail.com>
2021-06-29 23:00:55 +0200

    ♻️ - reorder session slides

--- R/utils.R
+++ R/utils.R
@@ -52,7 +52,7 @@ open_exercise <- function(session) {
   num <- stringr::str_pad(session, 2, pad = '0')
   ex_num <- glue::glue("ex_{stringr::str_pad(session, 2, pad = '0')}")
 
-  if (!(ex_num %in% c("03", "07", "08"))) {
+  if (!(num %in% c("03", "07", "08"))) {
     cli::cli_alert_danger(
       glue::glue("Session {num} does not have exercises! Choose Session 03, 07, or 08."))
   } else {
@@ -73,15 +73,15 @@ get_vignette_path <- function(...) {
 
 
 slides <- c(
-  "https://learnb4ss.github.io/slides/00_intro/index.html",
+  "https://learnb4ss.github.io/slides/00_intro/index.pdf",
   "https://learnb4ss.github.io/slides/01_a2r_initial_demo/index.html",
-  "https://learnb4ss.github.io/slides/02_bayes_theorem/index.html",
-  "https://learnb4ss.github.io/slides/03_a2r_prior_and_bayesian_updating/index.html",
-  "https://learnb4ss.github.io/slides/04_nhst_vs_bayesian_inference/index.html",
+  "https://learnb4ss.github.io/slides/02_bayes_theorem/index.pdf",
+  "https://learnb4ss.github.io/slides/03_a2r_priors_and_bayesian_updating/index.html",
+  "https://learnb4ss.github.io/slides/04_nhst_vs_bayesian_inference/pdf.html",
   "https://learnb4ss.github.io/slides/05_review/index.html",
   "https://learnb4ss.github.io/slides/06_a2r_inference_over_posterior/index.html",
-  "https://learnb4ss.github.io/slides/07_a2r_leveling_up/index.html",
-  "https://learnb4ss.github.io/slides/08_more_priors_likelihoods/index.html",
+  "https://learnb4ss.github.io/slides/07_more_priors/index.html",
+  "https://learnb4ss.github.io/slides/08_a2r_leveling_up/index.pdf",
   "https://learnb4ss.github.io/slides/09_sample_the_posterior/index.html",
   "https://learnb4ss.github.io/slides/10_wrap_up/index.html"
 )
